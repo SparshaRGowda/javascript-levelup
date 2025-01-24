28abd68721922f478aa50a75396534f68db1349e
Sparsha Ramakrishna <32491531+SparshaRGowda@users.noreply.github.com>
2025-01-24 23:04:46 +0000

    movie-object

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,16 @@
 // Write your code here
+function Movie(title, director, genre, releaseYear, rating){
+  this.title = title;
+  this.director = director;
+  this.genre = genre;
+  this.releaseYear = releaseYear;
+  this.rating = rating;
+}
+
+Movie.prototype.getOverview = function(){
+  return`${this.title}, a ${this.genre} film directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}.`
+}
+
+const mov = new Movie("hum", "sam", "romance", 1997, 4.5);
+console.log(mov);
+console.log(mov.getOverview());
