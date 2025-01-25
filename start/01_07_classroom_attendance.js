89227207ceac3eb21f3b4c32650ae236808cb69b
Sparsha Ramakrishna <32491531+SparshaRGowda@users.noreply.github.com>
2025-01-25 20:41:39 +0000

    object-and-array-destructuring

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,25 @@
 // Write your code here
+const classroom = {
+  hasTeachingAssistant: false,
+  classList: ['Sam', 'Stu1', 'Stu2', 'Stu3']
+};
+function getStudents(classR){
+  let {hasTeachingAssistant, classList} = classR;
+  let teacher, assistant, students;
+  console.log(hasTeachingAssistant);
+  if(hasTeachingAssistant){
+    [teacher, assistant, ...students] = classList;
+  }else{
+    [teacher, ...students] = classList;
+  }
+  return students;
+  // let {hasTeachingAssistant, classList} = classR;
+  // let [v1, v2, ...v3] = classList;
+  // if(hasTeachingAssistant){
+  //   console.log([...v3]);
+  // }
+  // else{
+  //   console.log([v2, ...v3]);
+  // }
+}
+console.log(getStudents(classroom));
