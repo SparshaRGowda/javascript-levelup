ae64249f6a2feb266910445eb4fd1511a4752366
Sparsha Ramakrishna <32491531+SparshaRGowda@users.noreply.github.com>
2025-01-25 19:31:40 +0000

    symbols

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,11 @@
 // Write your code here
+const userName = Symbol('user');
+const password  = Symbol('pass');
+const user = {
+  [userName]: 'sparsha', //adding symbols as object keys([]) will prevent from accessing
+  [password]: 'spar',
+  age: 34
+}
+console.log(user.userName);
+console.log(user.password);
+console.log(user.age);
