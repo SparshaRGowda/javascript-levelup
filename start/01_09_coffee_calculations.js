c6700e01a40d659efef69045484e55b4e7faa57d
Sparsha Ramakrishna <32491531+SparshaRGowda@users.noreply.github.com>
2025-01-26 18:58:13 +0000

    coffee-total-reduce

--- start/01_09_coffee_calculations.js
+++ start/01_09_coffee_calculations.js
@@ -1,1 +1,6 @@
 // Write your code here
+function calculate(arr){
+  const total = arr.reduce((acc, curr)=> acc += curr, 0)
+  return `The total bill is ${total*1.25}`;
+}
+console.log(calculate([2,3,1,5]));
