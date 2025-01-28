cbdee27a481ad8fa37c9d73323e39b1c04feb771
Sparsha Ramakrishna <32491531+SparshaRGowda@users.noreply.github.com>
2025-01-28 00:43:11 +0000

    string-permutation-and-api-fetch

--- start/01_12_string_permutation.js
+++ start/01_12_string_permutation.js
@@ -1,1 +1,20 @@
 // Write your code here
+//iterative
+function permutation(str){
+  let res; 
+  for(let i=1; i<=str.length; i++){
+    if(res === undefined){
+      res = i;
+      continue;
+    }
+    res *= i;
+  }
+  return res;
+}
+//recursive
+function permu(str){
+  if(str.length === 1) return 1;
+  return str.length * permu(str.slice(1));
+}
+console.log(permutation('hello'));
+console.log(permu('hello'));
