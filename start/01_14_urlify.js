0c1c48ba9afc2673b855f7b3c5ee47e9e39e39c1
Sparsha Ramakrishna <32491531+SparshaRGowda@users.noreply.github.com>
2025-01-28 01:01:53 +0000

    strings-regex-urlify

--- start/01_14_urlify.js
+++ start/01_14_urlify.js
@@ -1,1 +1,6 @@
 // Write your code here
+function urlify(str){
+  const reg = /[.,/#!$%^&*;:{}=!\-_`~()'"]/g; //whitespace is not included in this
+  return str.replace(reg, "").toLowerCase().trim().replace(" ", "-");
+}
+console.log(urlify("Sparsha Ram"));
