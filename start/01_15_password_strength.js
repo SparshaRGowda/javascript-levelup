2e995e67ecaa799a1c8ee123e80d6e057d68cc68
Sparsha Ramakrishna <32491531+SparshaRGowda@users.noreply.github.com>
2025-01-28 01:20:58 +0000

    password-regex

--- start/01_15_password_strength.js
+++ start/01_15_password_strength.js
@@ -1,1 +1,11 @@
 // Write your code here
+function pass(password){
+  let oneLowerCaseLetter = /(?=.+[a-z])/;
+  let oneUpperCaseLetter = /(?=.+[A-Z])/;
+  let oneDigit = /(?=.+[0-9])/;
+  let oneSpecialCharacter = /(?=.+[!@#$%^&*])/;
+  let minimumEightCharacters = /(?=.{8,})/;
+  let isValid = oneLowerCaseLetter.test(password) && oneUpperCaseLetter.test(password) && oneDigit.test(password) && oneSpecialCharacter.test(password) && minimumEightCharacters.test(password);
+  return isValid ? "Your password is valid" : "Your password is invalid";
+}
+console.log(pass("9Ab!45679"));
